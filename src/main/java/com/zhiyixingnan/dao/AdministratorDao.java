78e9c4c77b7e0101f21a253eaf0211d332bd5313
JingZz7 <1950662032@qq.com>
2022-10-25 19:10:27 +0800

    chore(AdministratorDao): add

---
+++ src/main/java/com/zhiyixingnan/dao/AdministratorDao.java
@@ -0,0 +1,9 @@
+package com.zhiyixingnan.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhiyixingnan.domain.Administrator;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AdministratorDao extends BaseMapper<Administrator> {
+}
