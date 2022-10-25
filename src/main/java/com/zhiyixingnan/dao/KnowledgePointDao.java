806316bc0f092c5b668b1c4af5fbe9aa226cd4cc
JingZz7 <1950662032@qq.com>
2022-10-25 19:28:36 +0800

    chore(KnowledgePointDao): create

---
+++ src/main/java/com/zhiyixingnan/dao/KnowledgePointDao.java
@@ -0,0 +1,9 @@
+package com.zhiyixingnan.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhiyixingnan.domain.KnowledgePoint;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface KnowledgePointDao extends BaseMapper<KnowledgePoint> {
+}
