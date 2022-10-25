cd1437daa053bf7ce7278f26b5f9ce5e7118b0bb
JingZz7 <1950662032@qq.com>
2022-10-25 18:56:25 +0800

    chore(TeacherDao): add TeacherDao

---
+++ src/main/java/com/zhiyixingnan/dao/TeacherDao.java
@@ -0,0 +1,9 @@
+package com.zhiyixingnan.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhiyixingnan.domain.Teacher;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TeacherDao extends BaseMapper<Teacher> {
+}
