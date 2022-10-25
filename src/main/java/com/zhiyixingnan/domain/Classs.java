b1e18d69d7d8c881b17965ea9d14bb1e01823501
JingZz7 <1950662032@qq.com>
2022-10-25 19:06:37 +0800

    refactor(Classs): Primary key mapping modification

--- src/main/java/com/zhiyixingnan/domain/Classs.java
+++ src/main/java/com/zhiyixingnan/domain/Classs.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Classs {
 
     @TableId(value = "pk_class_id",type = IdType.AUTO)
-    private BigInteger PKTClass;
+    private BigInteger PKClassId;
 
     @TableField("id")
     private String id;
