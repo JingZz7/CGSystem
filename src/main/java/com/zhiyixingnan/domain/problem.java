ab97685f563196a3317dac5c5721741b1123a2fa
JingZz7 <1950662032@qq.com>
2022-10-25 19:38:01 +0800

    chore(problem): create

---
+++ src/main/java/com/zhiyixingnan/domain/problem.java
@@ -0,0 +1,36 @@
+package com.zhiyixingnan.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Data
+@TableName("t_problem")
+public class problem {
+
+    @TableId(value = "pk_problem_id",type = IdType.AUTO)
+    private BigInteger PKProblemId;
+
+    @TableField("id")
+    private String id;
+
+    @TableField("knowledge_point_id")
+    private String KnowledgePointId;
+
+    @TableField("name")
+    private String name;
+
+    @TableField("difficulty")
+    private char difficulty;
+
+    @TableField("label")
+    private String label;
+
+    @TableField(exist = false)
+    private List<problem> problemList;
+}
