032eefa6c46d8287ec83e60c93973bcf034b4e3e
JingZz7 <1950662032@qq.com>
2022-10-25 23:49:34 +0800

    build(CommentTeacherDao): create

---
+++ src/main/java/com/zhiyixingnan/dao/CommentTeacherDao.java
@@ -0,0 +1,8 @@
+package com.zhiyixingnan.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhiyixingnan.domain.CommentTeacher;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommentTeacherDao extends BaseMapper<CommentTeacher> {}
