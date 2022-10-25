9bb182500f6f57d69c40943afe8133d03660674e
JingZz7 <1950662032@qq.com>
2022-10-25 19:27:56 +0800

    chore(KnowledgePoint): create

---
+++ src/main/java/com/zhiyixingnan/domain/KnowledgePoint.java
@@ -0,0 +1,26 @@
+package com.zhiyixingnan.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigInteger;
+
+@Data
+@TableName("t_knowledge_point")
+public class KnowledgePoint {
+
+    @TableId(value = "pk_knowledge_point_id",type = IdType.AUTO)
+    private BigInteger PKKnowledgePointId;
+
+    @TableField("id")
+    private String id;
+
+    @TableField("name")
+    private String name;
+
+    @TableField("student_id")
+    private String StudentId;
+}
