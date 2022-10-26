4461fe1cdf4ae4affb4625c46d66ca4216e7967c
JingZz7 <1950662032@qq.com>
2022-10-26 15:48:05 +0800

    feat(ModelOutputScore): create new entity

---
+++ src/main/java/com/zhiyixingnan/domain/ModelOutputKScore.java
@@ -0,0 +1,23 @@
+package com.zhiyixingnan.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Data
+@TableName("t_model_output_score")
+public class ModelOutputKScore {
+
+  @TableId(value = "pk_model_output_score_id", type = IdType.AUTO)
+  private BigInteger PKModelOutputScore;
+
+  @TableField("student_id")
+  private String studentId;
+
+  @TableField("exam_score")
+  private BigDecimal examScore;
+}
