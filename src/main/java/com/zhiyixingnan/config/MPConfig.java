87bb92a1cb722c2c417445106b84e4551fad88cf
JingZz7 <1950662032@qq.com>
2022-10-26 13:46:54 +0800

    chore(MPConfig): Configure the interceptor , mainly used for paging functions

--- src/main/java/com/zhiyixingnan/config/MPConfig.java
+++ src/main/java/com/zhiyixingnan/config/MPConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MPConfig {
+  /**
+   * @author ZJ Description MyBatisPlus拦截器 date 2022-10-26 13:45:26 13:45
+   * @param
+   */
   @Bean
   public MybatisPlusInterceptor mybatisPlusInterceptor() {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
