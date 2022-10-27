769a912fa67167da08881b309bbe2e0fcaffa196
JingZz7 <1950662032@qq.com>
2022-10-27 18:05:17 +0800

    feat(Some service function): Some new service operations have been added

---
+++ src/main/java/com/zhiyixingnan/service/IKnowledgePointService.java
@@ -0,0 +1,6 @@
+package com.zhiyixingnan.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhiyixingnan.domain.KnowledgePoint;
+
+public interface IKnowledgePointService extends IService<KnowledgePoint> {}
