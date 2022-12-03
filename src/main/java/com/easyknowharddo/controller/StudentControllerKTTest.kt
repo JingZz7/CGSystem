cdc5852a85f7d7e32cc79192c35e3547edeb0cb7
JingZz7 <1950662032@qq.com>
2022-12-03 19:30:41 +0800

    feat(PersonalCenterController): Display of personal information (personal center)

--- src/main/java/com/easyknowharddo/controller/StudentControllerKTTest.kt
+++ src/main/java/com/easyknowharddo/controller/StudentControllerKTTest.kt
@@ -6,7 +6,14 @@ import com.easyknowharddo.controller.utils.Result
 import com.easyknowharddo.domain.Student
 import com.easyknowharddo.service.IStudentService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/students")
